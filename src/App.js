0831fd18c09e31954fc0e1cf037c2c4cacc47043
jeff-burns <1jeffreyburns@gmail.com>
2018-12-11 14:02:37 -0700

    cleaned up code and styling, also added loading progress bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import Header from './components/Header/index';
-import BeerCards from './components/BeerCards/index'
+import Header from "./components/Header/index";
+import BeerCards from "./components/BeerCards/index";
 
 const beerURL = "https://beer.fluentcloud.com/v1/beer";
 const proxyURL = "https://cors-anywhere.herokuapp.com/";
-
 
 class App extends Component {
   constructor() {
@@ -12,17 +11,16 @@ class App extends Component {
     this.state = {
       allBeers: [],
       addedBeer: "",
-      selectedBeer: []
+      loading: true
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.addBeerClick = this.addBeerClick.bind(this);
-
   }
 
   handleChange(event) {
     event.preventDefault();
-    const value = event.target.value;    
+    const value = event.target.value;
     this.setState({
       addedBeer: value
     });
@@ -32,68 +30,42 @@ class App extends Component {
     event.preventDefault();
     const beerName = this.state.addedBeer;
     const addedBeerPostBody = {
-      "name": beerName,
-      "likes": "0"
-    }
-    console.log(beerName, addedBeerPostBody)
-    fetch((proxyURL + beerURL)
-    , {
-      method: 'POST',
+      name: beerName,
+      likes: "0"
+    };
+    console.log(beerName, addedBeerPostBody);
+    fetch(proxyURL + beerURL, {
+      method: "POST",
       headers: new Headers({
         "content-type": "application/json"
       }),
       body: JSON.stringify(addedBeerPostBody)
     })
-    // // .then((response) => response.json())
-    // .then(function(response) {
-    //   console.log(response);
-    // });
-    .then(response => {
-      console.log(response);
-      if(response.ok) {
-        return response.json();
-      } else {
-        const contentType = response.headers.get('content-type');
-        if (contentType.startsWith('application/json')) {
-          return response.json().then(json => {
-            const error = new Error(json.error.message);
-            throw error;
-          });
+      .then(response => {
+        console.log(response);
+        if (response.ok) {
+          return response.json();
         } else {
-          const error = new Error(response.statusText);
-          throw error;
+          const contentType = response.headers.get("content-type");
+          if (contentType.startsWith("application/json")) {
+            return response.json().then(json => {
+              const error = new Error(json.error.message);
+              throw error;
+            });
+          } else {
+            const error = new Error(response.statusText);
+            throw error;
+          }
         }
-      }
-    })
-    .then(json => {
-      console.log(json);
-      
-    })
-    .catch(error => {
-      console.error('error', error.message);
-      // show an error message
-    });
-  
-
+      })
+      .then(json => {
+        console.log(json);
+      })
+      .catch(error => {
+        console.error("error", error.message);
+        // show an error message
+      });
   }
-
-  // likeClick = (beerName) => {
-  //   console.log("you clicked me!")
-  //   console.log(beerName)
-  // //   let beerNames = 
-  // //     this.state.allBeers
-  // //   let newState = beerNames.filter(beer => {
-       
-  // //       //if true current gets returned to new array
-  // //       //if false it gets taken out
-  // //       return !(beerName === beer.name)
-  // //     })
-    
-  // //   this.setState({
-  // //     modification: beerName,
-  // //     data: newState
-  // //   })
-  // }
 
   componentDidMount() {
     fetch(proxyURL + beerURL)
@@ -102,89 +74,79 @@ class App extends Component {
       })
       .then(response => {
         console.log(response);
-        this.setState({ allBeers: response })
+        this.setState({ allBeers: response });
       })
-      .catch(() => console.log("Can’t access " + beerURL + " response. Blocked by browser?"))
+      .catch(() =>
+        console.log(
+          "Can’t access " + beerURL + " response. Blocked by browser?"
+        )
+      );
+      this.setState({ loading: false })
   }
 
-  
-
   render() {
-    console.log(this.state)
-
+    console.log(this.state);
+    if (this.state.allBeers.length === 0) {
+      return <div class="progress">
+      <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{ width: 300 }}></div>
+    </div>;
+    }
     const beerSections = this.state.allBeers.map(beer => {
-      return <BeerCards 
-          id={beer.id} 
-          name={beer.name} 
-          likes={beer.likes} 
+      return (
+        <BeerCards
+          id={beer.id}
+          name={beer.name}
+          likes={beer.likes}
           likeClick={this.likeClick}
-      />;
+        />
+      );
     });
 
     return (
       <div>
         <Header />
         <main className="container">
-      <section className=" column">
-        <div className="card mb-3">
-          <h2 className="card-header">Beer Cooler</h2>
-          <div className="card-body">
-            <h3 className="card-title">Beers</h3>
-          </div>
-          <ul className="list-group list-group-flush">{beerSections}</ul>
-        </div>
-      </section>
-      <section className=" column">
-        <div className="card mb-3">
-          <div className="card-body">
-            <div className="form-group">
-              <label
-                className="col-form-label col-form-label-sm"
-                htmlFor="inputSmall"
-              >
-                Add A Beer!
-              </label>
-              <input
-                className="form-control form-control-sm"
-                type="text"
-                placeholder="Budweiser"
-                id="inputSmall addedBeer"
-                name="addedBeer"
-                value={this.state.addedBeer}
-                onChange={this.handleChange}
-              />
-              <button type="button" className="btn btn-primary btn-sm" onClick= { this.addBeerClick}
-              >Add Beer!</button>             
+          <section className="row">
+            <div className="card mb-3 col-sm-4">
+              <h2 className="card-header text-warning font-weight-bold">Beer Cooler</h2>
+              <div className="card-body">
+                <h3 className="card-title font-weight-bold">Beers</h3>
+              </div>
+              <ul className="list-group list-group-flush">{beerSections}</ul>
             </div>
-          </div>
-        </div>
-      </section>
-    </main>
-        
+            <div className="card mb-3 col-sm-4">
+              <div className="card-body">
+                <div className="form-group">
+                  <label
+                    className="col-form-label col-form-label-sm text-danger"
+                    htmlFor="inputSmall"
+                  >
+                    Add A Beer!
+                  </label>
+                  <input
+                    className="form-control form-control-sm"
+                    type="text"
+                    placeholder="Budweiser"
+                    id="inputSmall addedBeer"
+                    name="addedBeer"
+                    value={this.state.addedBeer}
+                    onChange={this.handleChange}
+                  />
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-sm"
+                    onClick={this.addBeerClick}
+                  >
+                    Add Beer!
+                  </button>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
       </div>
     );
   }
 }
 
 export default App;
-
-// <Main beers={this.state.allBeers}
-//               addBeerClick={this.addBeerClick}
-//               likeClick={this.likeClick}
-//               value={this.state.addedBeer}
-//               handleChange={this.handleChange}
-//         />
-
-
-
-
-// <header className="App-header">
-//           <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-//             <a className="navbar-brand" href="#">
-//               Navbar
-//             </a>
-//           </nav>
-//         </header>
-//         <main className="container">
-//           <h2>Show Beers Here</h2>
-//         </main>
--- src/components/BeerCards/index.js
+++ src/components/BeerCards/index.js
@@ -1,72 +1,55 @@
 import React, { Component } from "react";
 
-
 const proxyURL = "https://cors-anywhere.herokuapp.com/";
 
 class BeerCards extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        selectedBeerId: this.props.id, 
-        selectedBeerLikes: this.props.likes
+      selectedBeerId: this.props.id,
+      selectedBeerLikes: this.props.likes
     };
 
     this.likeClick = this.likeClick.bind(this);
   }
 
-//   likeClick() {
-//     // this.setState({ selectedBeerId: this.props.id, selectedBeerLikes: this.props.likes })
-//     this.updateLikes();
-//   }
-
   likeClick() {
     const currentLikes = this.state.selectedBeerLikes;
-    const newLikes = (currentLikes + 1);
+    const newLikes = currentLikes + 1;
     const addingLikePutBody = {
-        "id": this.state.selectedBeerId,
-        "likes": newLikes
-    }
-    console.log(currentLikes, newLikes, addingLikePutBody)
+      id: this.state.selectedBeerId,
+      likes: newLikes
+    };
+    console.log(currentLikes, newLikes, addingLikePutBody);
 
-    fetch((proxyURL + `https://beer.fluentcloud.com/v1/beer/${this.state.selectedBeerId}`)
-    , {
-      method: 'PUT',
-      headers: new Headers({
-        "content-type": "application/json"
-      }),
-      body: JSON.stringify(addingLikePutBody)
-    })
-    .then(response => {
+    fetch(
+      proxyURL +
+        `https://beer.fluentcloud.com/v1/beer/${this.state.selectedBeerId}`,
+      {
+        method: "PUT",
+        headers: new Headers({
+          "content-type": "application/json"
+        }),
+        body: JSON.stringify(addingLikePutBody)
+      }
+    )
+      .then(response => {
         console.log(response);
-        if(response.ok) {
-          return response.json();
-        } else {
-          const contentType = response.headers.get('content-type');
-          if (contentType.startsWith('application/json')) {
-            return response.json().then(json => {
-              const error = new Error(json.error.message);
-              throw error;
-            });
-          } else {
-            const error = new Error(response.statusText);
-            throw error;
-          }
-        }
+        return response.json();
       })
       .then(json => {
         console.log(json);
-        
       })
       .catch(error => {
-        console.error('error', error.message);
+        console.error("error", error.message);
         // show an error message
       });
   }
   render() {
-      console.log(this.state)
+    console.log(this.state);
     return (
       <li className="list-group-item" key={this.props.id}>
-        <h5 className="card-title">{this.props.name}</h5>
+        <h5 className="card-title text-info">{this.props.name}</h5>
         <button
           type="button"
           className="btn btn-primary btn-sm"
@@ -80,13 +63,3 @@ class BeerCards extends Component {
 }
 
 export default BeerCards;
-
-// onClick= { props.likeClick.bind(null, this.props.name) }
-
-//   <div className="card col-xs-12 col-sm-6 col-md-4" key={props.id}>
-//             <div className="card-body">
-//               <h5 className="card-title">{props.name}</h5>
-//               <p className="card-text">{props.likes}</p>
-//             </div>
-//           </div>
-//         <a className="card-text">Like This Beer {props.likes}</a>
